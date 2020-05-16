e1a7c291b885e5ee1522c99011b5876a6a63e422
ilyailyushko <65430523+ilyailyushko@users.noreply.github.com>
2020-05-17 02:53:50 +0300

    555

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot("1083239056:AAHazdekMaGQZ5uReSWKh2J412g8378da-E")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Howdy, hoототлотолтлотлw are you doing?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
